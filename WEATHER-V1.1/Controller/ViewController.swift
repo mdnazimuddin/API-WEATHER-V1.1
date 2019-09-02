af228b3f6745331ee59ae6635bfa52dee4eed014
Nazim Uddin <mdnazimuddincse@gmail.com>
2019-09-02 08:03:35 +0600

    Create REST API Weather App Version 1.0

---
+++ WEATHER-V1.1/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WEATHER-V1.1
+//
+//  Created by Nazim Uddin on 2/9/19.
+//  Copyright © 2019 Nazim Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getWeatherData()
+    }
+    func getWeatherData(){
+        
+    }
+
+
+}
+
